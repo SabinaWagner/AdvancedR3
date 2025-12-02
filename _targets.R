01e3310d0055607e6afa82546b3a4df407599b2b
Sabina Wagner <wagn@sdu.dk>
2025-12-02 16:02:09 +0100

    added quarto to target

--- _targets.R
+++ _targets.R
@@ -5,7 +5,7 @@
 
 # Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed.
+library(tarchetypes) # Load other packages as needed.
 
 # Set target options:
 tar_option_set(
@@ -58,5 +58,9 @@ list(
   tar_target(
     name = lipidomics,
     command = readr::read_csv(file, show_col_types = FALSE)
+  ),
+  tar_quarto(
+    name = quarto_doc,
+    path = "docs/learning.qmd"
   )
 )
