1954b5750ae064b823f8ee0a5b76e0bb6a72006e
Sabina Wagner <wagn@sdu.dk>
2025-12-03 11:20:47 +0100

    small edits to table and plot

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,3 @@
-
 #' create descriptive table
 #'
 #' @param data a dataset containing the columns 'metabolite' and 'value'
@@ -8,10 +7,13 @@
 create_table_descriptive_stats <- function(data) {
   data |>
     dplyr::group_by(metabolite) |>
-    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd, median = median, iqr = IQR))) |>
     dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) format(x, digits = 2))) |>
-    dplyr::mutate(MeanSD = glue::glue("{value_mean} ± ({value_sd})")) |>
-    dplyr::select(Metabolite = metabolite, "Mean ± (SD)" = MeanSD)
+    dplyr::mutate(
+      MeanSD = glue::glue("{value_mean} ± ({value_sd})"),
+      MedianIQR = glue::glue("{value_median} ± ({value_iqr})")
+    ) |>
+    dplyr::select(Metabolite = metabolite, "Mean ± (SD)" = MeanSD, "Median ± (IQR)" = MedianIQR)
 }
 
 
@@ -26,5 +28,9 @@ create_plot_distributions <- function(data) {
     ggplot2::ggplot(ggplot2::aes(x = value)) +
     ggplot2::geom_histogram() +
     ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free") +
-    ggplot2::theme_minimal()
+    ggplot2::theme_classic() +
+    ggplot2::labs(
+      x = "Value",
+      y = "Count"
+    )
 }
