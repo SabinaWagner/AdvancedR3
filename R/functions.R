addc8071056ff01b77e758d0e63daedf4ba66f14
Sabina Wagner <wagn@sdu.dk>
2025-12-03 09:54:23 +0100

    edited descriptive table function and added tidyselect to packages in description

--- R/functions.R
+++ R/functions.R
@@ -8,8 +8,8 @@
 create_table_descriptive_stats <- function(data) {
   data |>
     dplyr::group_by(metabolite) |>
-    dplyr::summarise(across(value, list(mean = mean, sd = sd))) |>
-    dplyr::mutate(across(where(is.numeric), \(x) format(x, digits = 2))) |>
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) format(x, digits = 2))) |>
     dplyr::mutate(MeanSD = glue::glue("{value_mean} ± ({value_sd})")) |>
     dplyr::select(Metabolite = metabolite, "Mean ± (SD)" = MeanSD)
 }
